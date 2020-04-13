d6a70f32278368f84128a9749b3c5ddbdfd5e2fb
chem1st <mikegorlov@gmail.com>
2020-04-13 18:36:17 +0300

    create_superuser() fixed
    username is optional now

--- klio/users/models.py
+++ klio/users/models.py
@@ -1,10 +1,49 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from cities_light.models import City, Country
 
 from contacts.models import Phone
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name, profile_picture, password=None,
+                    is_admin=False, is_staff=False, is_active=True):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User must have a password")
+        if not full_name:
+            raise ValueError("User must have a full name")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.full_name = full_name
+        user.set_password(password)  # change password to hash
+        user.profile_picture = profile_picture
+        user.admin = is_admin
+        user.staff = is_staff
+        user.active = is_active
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, is_active=True, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User must have a password")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.admin = True
+        user.staff = True
+        user.active = is_active
+        user.save(using=self._db)
+        return user
 
 
 # https://habrahabr.ru/post/313764/
@@ -28,6 +67,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     class Meta:
         ordering = ['-is_active', 'last_name', 'username']
